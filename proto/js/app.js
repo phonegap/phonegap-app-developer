41f14730f6313a4342822450b419da93730cc2f0
Yohei Shimomae <yohei.shimomae@gmail.com>
2013-04-19 16:41:28 -0700

    added modal UI for use for authentication etc.

--- proto/js/app.js
+++ proto/js/app.js
@@ -1,22 +1,85 @@
+/*---------------------------------------------------
+    General
+---------------------------------------------------*/
+
 $().ready(function() {
 
-    $('#sync .bt').on('click', onBtSync);
-    $('#build .bt').on('click', onBtBuild);
+    // Add Events
+    $('#sync .bt').on('click', openSync);    
+    $('#build .bt').on('click', openBuild);
+
+    $('#sync form').submit(syncSubmit);
+    $('#build form').submit(buildSubmit);
 });
 
-function onBtSync() {
-    showSection('sync');
-}
 
-function onBtBuild() {
-    showSection('build');
-}
+/*---------------------------------------------------
+    UI - General
+---------------------------------------------------*/
 
-function showSection( type ) {
-    $('.enlarge').removeClass('enlarge');
-    $('#' + type).addClass('enlarge');
-
+function showSpinnerModal(label) {
+    var el = $('<dialog>')
+        .append($('<figure>', {'class':'icon-spinner'}))
+        .append($('<label>', {text:label}));
+    $('body').append(el);
     setTimeout(function() {
-        $('#' + type + ' input').first().focus().click();
-    }, 200);
+        el.css({opacity:1});
+    }, 50);
+
+    // For demo purpose, hide after 2 seconds
+    setTimeout(function() {
+        hideSpinnerModal();
+    }, 2000);
 }
+
+function hideSpinnerModal() {
+    $('dialog').css({opacity:0})
+    setTimeout(function() {
+        $('dialog').remove();
+    }, 500);
+}
+
+
+/*---------------------------------------------------
+    UI - Accordion
+---------------------------------------------------*/
+
+function openSync() {
+    openSection('sync');
+}
+
+function openBuild() {
+    openSection('build');
+}
+
+function openSection(type) {
+    var isOpen = $('#' + type).hasClass('open');
+    $('.open').removeClass('open');
+
+    if (!isOpen) {
+        $('#' + type).addClass('open');
+
+        setTimeout(function() {
+            $('#' + type + ' input').first().focus().click();
+            // This does not work properly on iOS
+        }, 200);
+    }
+}
+
+
+/*---------------------------------------------------
+    UI - Form
+---------------------------------------------------*/
+
+function syncSubmit() {
+    var ip = $('#sync-ip').attr('value');
+    showSpinnerModal('Looking for ' + ip);
+    return false;
+}
+
+function buildSubmit() {
+    showSpinnerModal('Signing in');
+    return false;
+}
+
+
