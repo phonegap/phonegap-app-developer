9a412eed84935a0807f894f1600875074a1a2cdd
Michael Brooks <michael@michaelbrooks.ca>
2014-01-22 14:47:23 -0800

    [#28] Add network API.

--- platforms/wp8/.staging/www/cordova_plugins.js
+++ platforms/wp8/.staging/www/cordova_plugins.js
@@ -240,6 +240,21 @@ module.exports = [
         "clobbers": [
             "navigator.compass"
         ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.network-information/www/network.js",
+        "id": "org.apache.cordova.network-information.network",
+        "clobbers": [
+            "navigator.connection",
+            "navigator.network.connection"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.network-information/www/Connection.js",
+        "id": "org.apache.cordova.network-information.Connection",
+        "clobbers": [
+            "Connection"
+        ]
     }
 ];
 module.exports.metadata = 
@@ -249,7 +264,8 @@ module.exports.metadata =
     "org.apache.cordova.device-motion": "0.2.4",
     "org.apache.cordova.camera": "0.2.6",
     "org.apache.cordova.media-capture": "0.2.6",
-    "org.apache.cordova.device-orientation": "0.3.4"
+    "org.apache.cordova.device-orientation": "0.3.4",
+    "org.apache.cordova.network-information": "0.2.6"
 }
 // BOTTOM OF METADATA
 });
