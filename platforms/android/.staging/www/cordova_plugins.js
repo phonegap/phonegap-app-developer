8ec0e4d1af10a02b031c7e1382d62ea45e362c5e
Michael Brooks <michael@michaelbrooks.ca>
2014-01-22 14:38:39 -0800

    [#28] Add camera API.

--- platforms/android/.staging/www/cordova_plugins.js
+++ platforms/android/.staging/www/cordova_plugins.js
@@ -135,13 +135,42 @@ module.exports = [
         "clobbers": [
             "navigator.accelerometer"
         ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.camera/www/CameraConstants.js",
+        "id": "org.apache.cordova.camera.Camera",
+        "clobbers": [
+            "Camera"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.camera/www/CameraPopoverOptions.js",
+        "id": "org.apache.cordova.camera.CameraPopoverOptions",
+        "clobbers": [
+            "CameraPopoverOptions"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.camera/www/Camera.js",
+        "id": "org.apache.cordova.camera.camera",
+        "clobbers": [
+            "navigator.camera"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.camera/www/CameraPopoverHandle.js",
+        "id": "org.apache.cordova.camera.CameraPopoverHandle",
+        "clobbers": [
+            "CameraPopoverHandle"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
     "org.apache.cordova.file": "0.2.5",
-    "org.apache.cordova.device-motion": "0.2.4"
+    "org.apache.cordova.device-motion": "0.2.4",
+    "org.apache.cordova.camera": "0.2.6"
 }
 // BOTTOM OF METADATA
 });
