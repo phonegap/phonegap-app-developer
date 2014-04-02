942088693fa1a82f74281a2c7d568a43d6cc0fbd
Michael Brooks <michael@michaelbrooks.ca>
2014-04-02 14:17:19 -0700

    [#55] Build app against Cordova Android 3.3.0.

---
+++ platforms/android/CordovaLib/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/mwbrooks/Development/lib/phonegap-app/platforms/android/CordovaLib/bin/AndroidManifest.xml : \
+/Users/mwbrooks/Development/lib/phonegap-app/platforms/android/CordovaLib/AndroidManifest.xml \
---
+++ platforms/android/CordovaLib/gen/R.java.d
@@ -0,0 +1,1 @@
+ : /Users/mwbrooks/Development/lib/phonegap-app/platforms/android/CordovaLib/bin/AndroidManifest.xml \
