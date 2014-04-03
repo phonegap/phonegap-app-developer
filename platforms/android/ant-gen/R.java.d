38a79d43bc8f276670c7b2a4cd516535723517b8
Michael Brooks <michael@michaelbrooks.ca>
2014-04-03 09:59:20 -0700

    [android] Build and test on 4.x devices.

--- platforms/android/ant-gen/R.java.d
+++ platforms/android/ant-gen/R.java.d
@@ -6,4 +6,9 @@
 /Users/mwbrooks/Development/lib/phonegap-app/platforms/android/res/drawable-xhdpi/icon.png \
 /Users/mwbrooks/Development/lib/phonegap-app/platforms/android/res/values/strings.xml \
 /Users/mwbrooks/Development/lib/phonegap-app/platforms/android/res/xml/config.xml \
+/Users/mwbrooks/Development/lib/phonegap-app/platforms/android/ant-build/res/drawable/icon.png \
+/Users/mwbrooks/Development/lib/phonegap-app/platforms/android/ant-build/res/drawable-hdpi/icon.png \
+/Users/mwbrooks/Development/lib/phonegap-app/platforms/android/ant-build/res/drawable-ldpi/icon.png \
+/Users/mwbrooks/Development/lib/phonegap-app/platforms/android/ant-build/res/drawable-mdpi/icon.png \
+/Users/mwbrooks/Development/lib/phonegap-app/platforms/android/ant-build/res/drawable-xhdpi/icon.png \
 /Users/mwbrooks/Development/lib/phonegap-app/platforms/android/ant-build/AndroidManifest.xml \
