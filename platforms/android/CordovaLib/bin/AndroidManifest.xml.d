8d3e81799a9b11800bf812a614942bf0c5e8bec8
Michael Brooks <michael@michaelbrooks.ca>
2014-04-17 10:04:36 -0700

    [git] Ignore Android platforms/android/CordovaLib/bin and /gen.

--- platforms/android/CordovaLib/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/mwbrooks/Development/lib/phonegap-app/platforms/android/CordovaLib/bin/AndroidManifest.xml : \
-/Users/mwbrooks/Development/lib/phonegap-app/platforms/android/CordovaLib/AndroidManifest.xml \
--- platforms/android/CordovaLib/gen/R.java.d
+++
@@ -1,1 +0,0 @@
- : /Users/mwbrooks/Development/lib/phonegap-app/platforms/android/CordovaLib/bin/AndroidManifest.xml \
