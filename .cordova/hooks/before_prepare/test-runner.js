88cef63337c7ca16de4cf6ac6503e7c689a2de77
Michael Brooks <michael@michaelbrooks.ca>
2014-05-09 08:19:15 -0700

    [#118] Add test runner hook to support --test flag.

---
+++ .cordova/hooks/before_prepare/test-runner.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+if (/--test/.test(process.env.CORDOVA_CMDLINE)) {
+    var fs = require('fs'),
+        path = require('path');
+
+    var dir = {
+        www: path.join(process.cwd(), 'www'),
+        test: path.join(process.cwd(), 'tests'),
+        backup: path.join(process.cwd(), 'www-backup')
+    };
+
+    // replace the test config.xml with the app config.xml
+    // we do this because we want to ensure our config is setup correctly.
+    var config = {
+        www: path.join(dir.www, 'config.xml'),
+        test: path.join(dir.test, 'config.xml')
+    };
+    fs.createReadStream(config.www).pipe(fs.createWriteStream(config.test));
+
+    // swap the test and www directories
+    fs.renameSync(dir.www, dir.backup);
+    fs.renameSync(dir.test, dir.www);
+}
