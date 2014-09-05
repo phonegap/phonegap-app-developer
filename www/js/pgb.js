373b310a61fa335c7c214d04595335c4a2959173
Ryan Willoughby <ryan.willoughby@adobe.com>
2014-09-04 18:47:52 -0700

    [www] pgb stuff

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -1,48 +1,120 @@
+(function() {
+ 
+$(document).on('deviceready', function() {
+	// Add slight delay to allow DOM rendering to finish.
+	// Avoids flicker on slower devices.
+	setTimeout(function() {
+   	// allow the screen to dim when returning from the served app
+   	window.plugins.insomnia.allowSleepAgain();
+   	navigator.splashscreen.hide();
+	});
+});
+
+
 var access_token;
+var apps = {};
 
 $("#login-form").on('submit', function(e) {
 	e.preventDefault();
 
-	var email = $(this).find("#email").val();
-	var pass = $(this).find("#password").val();
+	var email = $(this).find("#email").val(),
+			pass = $(this).find("#password").val();
 
 	cordova.exec(function(a) {
 		access_token = a.access_token;
 		getApps(a.access_token);
 	}, function(a) {
 		console.log("Auth failure: " + a.message);
+		alert('login failed');
 	}, "PhoneGapBuildOauth", "login", [email, pass]);
 
 });
 
-$("#login-form input[value='BACK']").on('click', function(e) {
+$("input[value='CONNECT']").on('click', function(e) {
 	e.preventDefault();
-	window.location.href='index.html'; 
+	window.location.href='connect.html'; 
 	return false;
 });
+
+$('.back').click(function() {
+	var cur = $('.view:not(.hidden)'),
+			prev = cur.prev('.view');
+	if (!!prev.length) {
+		cur.addClass('hidden');
+		prev.removeClass('hidden');
+	}
+})
+
+function showView(sel) {
+	$('.view').addClass('hidden');
+	$(sel).removeClass('hidden');
+}
 
 function getApps(access_token) {
 	$.ajax({
 	  dataType: "json",
 	  url:"https://build.phonegap.com/api/v1/apps?access_token=" + access_token,
-	  success: function(data) { 
+	  success: function(data) {
+	  	saveApps(data.apps);
 	  	renderApps(data.apps);
 	  }
 	});
 }
 
-function renderApps(apps) {
-	var list = $(".apps");
-	list.html("");
-	apps.forEach(function(app) {
-		list.append("<li>" + "<a href='#' onclick='install(\"" + app.id + "\")'>" + app.title + "</a></li>");
+function saveApps(appArray) {
+	appArray.forEach(function(app) {
+		apps[app.id] = app;
 	});
-	list.removeClass("hidden");
-	$('#login-form').addClass('hidden')
 }
 
-function install(id) {
-	var download_url = "https://build.phonegap.com/apps/" + id + 
+function renderApps(appArray) {
+	var list = $(".apps ul").html("");
+
+	appArray.forEach(function(app) {
+		var img_url = "https://build.phonegap.com/api/v1/apps/" + app.id + "/icon?access_token=" + access_token;
+		list.append("<li style='background-image: url(\"" + img_url + "\")'>" + 
+			"<a href='#' data-id='" + app.id + "'>" + app.title + "</a></li>");
+	});
+
+	list.find('a').click(renderApp);
+  showView('.apps');
+}
+
+function renderApp() {
+	var app = apps[$(this).attr("data-id")],
+			html = "<h2>" + app.title + "</h2><table>";
+
+	html += "<tr><td>Description</td><td>" + app.description + "</td></tr>";
+	html += "<tr><td>Phonegap</td><td>" + app.phonegap_version + "</td></tr>";
+	html += "<tr><td>Version</td><td>" + app.version + "</td></tr>";
+	if (app.repo)
+		html += "<tr><td>Repo</td><td><a class='link' href='#'>" + app.repo + "</a></td></tr>";
+	html += "</table>";
+	html += "<div class='btns'><input type='button' class='install' value='Install' data-id='" + app.id + "'/><br/>";
+	html += "<input type='button' class='run' value='Run' data-id='" + app.id + "'/></div>";
+
+	$('.app .detail').html(html)
+  $('.app input.install').click(install);
+  $('.app input.run').click(run);
+  $('.app a.link').click(browse);
+
+  showView('.app');
+}
+
+function browse() {
+	var url = $(this).text();
+	window.open(url, '_system');
+}
+
+function install() {
+	var id = $(this).attr("data-id"),
+			download_url = "https://build.phonegap.com/apps/" + id + 
 			"/download/" + device.platform.toLowerCase() + "?access_token=" + access_token;
 	window.open(download_url, "_system");
 }
+
+function run() {
+	console.log('Run not implemented yet ya jerk. ('+ $(this).attr("data-id") + ')');
+}
+
+})();
