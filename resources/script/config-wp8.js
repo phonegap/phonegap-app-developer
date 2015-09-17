02b29e23a3f2edc793a096d18f1d415b14ad9d3c
Michael Brooks <michael@michaelbrooks.ca>
2015-09-17 09:42:51 -0700

    [#348] Remove tabs and add semicolon.

--- resources/script/config-wp8.js
+++ resources/script/config-wp8.js
@@ -22,26 +22,26 @@ var wmappPath = path.join(projectRoot, 'platforms/wp8/Properties/WMAppManifest.x
 var parseString = xml2js.parseString;
 
 var overwriteWMAppManifest = function(xml) {
-	// delete then write
-	fs.unlink(wmappPath, function(){
-		fs.writeFile(wmappPath, xml.toString());
-	})
-}
+    // delete then write
+    fs.unlink(wmappPath, function(){
+        fs.writeFile(wmappPath, xml.toString());
+    });
+};
 
 var wmapp_file = fs.readFile(wmappPath, function(err, data) {
-			parseString(data, function(err, result) {
-					// fix App Title
-					result.Deployment.App[0]['$'].Title = 'PhoneGap Developer'
+            parseString(data, function(err, result) {
+                    // fix App Title
+                    result.Deployment.App[0]['$'].Title = 'PhoneGap Developer';
 
-					// fix productID
-					result.Deployment.App[0]['$'].ProductID = '{9E0C4412-1A96-4B19-A3F6-9D0CD6906F71}';
+                    // fix productID
+                    result.Deployment.App[0]['$'].ProductID = '{9E0C4412-1A96-4B19-A3F6-9D0CD6906F71}';
 
-					// fix TokenID
-					result.Deployment.App[0].Tokens[0].PrimaryToken[0]['$'].TokenID = 'PhoneGap';
+                    // fix TokenID
+                    result.Deployment.App[0].Tokens[0].PrimaryToken[0]['$'].TokenID = 'PhoneGap';
 
-					var builder = new xml2js.Builder();
-					var xmlOut = builder.buildObject(result);
-					overwriteWMAppManifest(xmlOut);
-				});
-			});
+                    var builder = new xml2js.Builder();
+                    var xmlOut = builder.buildObject(result);
+                    overwriteWMAppManifest(xmlOut);
+                });
+            });
 
