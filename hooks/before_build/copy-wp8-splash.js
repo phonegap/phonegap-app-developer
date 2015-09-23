b6b595b0f74b5d9b6b33572321a4b15086d703b9
Suraj Pindoria <suraj.pindoria@yahoo.com>
2015-09-23 12:08:18 -0700

    [#349] - better platform handling and cleaning up code

--- hooks/before_build/copy-wp8-splash.js
+++ hooks/before_build/copy-wp8-splash.js
@@ -2,10 +2,17 @@
 
 var fs = require('fs');
 
-var destPath = 'platforms/wp8/SplashScreenImage.jpg';
-var splashPath = 'resources/splash/wp8/SplashScreenImage.jpg';
+console.log('Running: Windows Phone splash screen flicker fix...');
+
+// only run script when building for WP8
+if (process.env['CORDOVA_PLATFORMS'] !== 'wp8') {
+    console.log('Skipping: the platform is not WP8');
+    process.exit(0);
+}
 
 // copy in our custom splash screen image
+var destPath = 'platforms/wp8/SplashScreenImage.jpg';
+var splashPath = 'resources/splash/wp8/SplashScreenImage.jpg';
 fs.createReadStream(splashPath).pipe(fs.createWriteStream(destPath));
 
 // disable the splash screen from loading its own splash image.
@@ -15,17 +22,14 @@ fs.createReadStream(splashPath).pipe(fs.createWriteStream(destPath));
 var splashscreenSrc = 'platforms/wp8/Plugins/cordova-plugin-splashscreen/SplashScreen.cs';
 fs.readFile(splashscreenSrc, 'utf8', function(err, data) {
     if (err) {
-        if (process.env['CORDOVA_PLATFORMS'] == 'wp8') {
-            console.log('Error reading the SplashScreen.cs file for WP8');
-            console.log('More info: <', err.message, '>');
-            process.exit(1);
-        }
-
-        process.exit(0);
+        console.log('Error reading the SplashScreen.cs file for WP8');
+        console.log('More info: <', err.message, '>');
+        process.exit(1);
     }
 
+    var result = '';
     if ((/return imageResource/).test(data)) {
-        var result = data.replace(/return imageResource/, 'return null');
+        result = data.replace(/return imageResource/, 'return null');
     } else {
         console.log('"copy-wp8-splash.js" needs to updated because of changes to cordova-plugin-splashscreen');
         process.exit(1);
