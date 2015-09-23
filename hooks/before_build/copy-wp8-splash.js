eb8b5a42e908c70af88b2ed519268046c4a92f67
Suraj Pindoria <suraj.pindoria@yahoo.com>
2015-09-23 10:50:55 -0700

    [#349] - Updated error handling and added additional comments

--- hooks/before_build/copy-wp8-splash.js
+++ hooks/before_build/copy-wp8-splash.js
@@ -8,24 +8,34 @@ var splashPath = 'resources/splash/wp8/SplashScreenImage.jpg';
 // copy in our custom splash screen image
 fs.createReadStream(splashPath).pipe(fs.createWriteStream(destPath));
 
-// find and replace line in WP splash screen plugin implementation
+// disable the splash screen from loading its own splash image.
+// this prevents a quick flicker between the native splash screen and the fake splash screen
+// displayed by the Cordova splash screen plugin.
+// see: https://github.com/phonegap/phonegap-app-developer/issues/349#issuecomment-141577196
 var splashscreenSrc = 'platforms/wp8/Plugins/cordova-plugin-splashscreen/SplashScreen.cs';
 fs.readFile(splashscreenSrc, 'utf8', function(err, data) {
-    if (err)
-        return 0;
+    if (err) {
+        if (process.env['CORDOVA_PLATFORMS'] == 'wp8') {
+            console.log('Error reading the SplashScreen.cs file for WP8');
+            console.log('More info: <', err.message, '>');
+            process.exit(1);
+        }
+
+        process.exit(0);
+    }
 
     if ((/return imageResource/).test(data)) {
         var result = data.replace(/return imageResource/, 'return null');
     } else {
         console.log('"copy-wp8-splash.js" needs to updated because of changes to cordova-plugin-splashscreen');
-        return 1;
+        process.exit(1);
     }
 
     fs.writeFile(splashscreenSrc, result, 'utf8', function(err) {
         if (err) {
             console.log('Error while writting to SplashScreen.cs to disable splash screen on WP8');
             console.log('More info: <', err.message, '>');
-            return 1;
+            process.exit(1);
         }
     });
 });
