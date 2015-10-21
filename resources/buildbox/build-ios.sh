f3c76866c8c8097fb5ff46b8242c475c8ca5d6d4
timkim <timk@adobe.com>
2015-10-21 15:34:58 -0700

    [ci] - debugging failing signing issue

--- resources/buildbox/build-ios.sh
+++ resources/buildbox/build-ios.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 git clone https://github.com/phonegap/phonegap-app-developer-keys.git
+ls resources/signing/ios
 ln -s phonegap-app-developer-keys/keys/ios resources/signing/ios
 npm run -- release-ios
