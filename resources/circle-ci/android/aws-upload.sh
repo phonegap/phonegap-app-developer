43274e06736b58006acf5ae4fca031a057b8583c
Suraj Pindoria <suraj.pindoria@yahoo.com>
2015-10-21 14:48:02 -0700

    [ci] - add script to upload android binary to S3

---
+++ resources/circle-ci/android/aws-upload.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+if [ "$CIRCLECI" != true ]; then
+    echo 'Skipping: build system is not CircleCI'
+    exit
+fi
+
+APKLOCATION=/platforms/android/build/outputs/apk/android-release.apk
+
+aws configure set aws_access_key_id $AWSACCESSKEY
+aws configure set aws_secret_access_key $AWSSECRETKEY
+aws configure set default.region us-east-1
+aws configure set default.output json
+
+aws s3 cp $APKLOCATION s3://ci.phonegap.com/phonegap-app-developer/android/Phonegap-$CIRCLE_SHA1.apk
