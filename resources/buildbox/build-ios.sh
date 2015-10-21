650e5620643ecd49210cc0f8e09fe6b1835e52d3
timkim <timk@adobe.com>
2015-10-21 15:42:38 -0700

    [ci] - debugging buildkite

--- resources/buildbox/build-ios.sh
+++ resources/buildbox/build-ios.sh
@@ -2,6 +2,6 @@
 git clone https://github.com/phonegap/phonegap-app-developer-keys.git
 ln -s phonegap-app-developer-keys/keys/ios ./resources/signing/ios
 ls
-ls ./resources
-ls resources
+ls ./resources/signing/ios
+
 npm run -- release-ios
