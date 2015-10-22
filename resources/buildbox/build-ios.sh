5cac3ae60ec559989d27564d2f778354dcd3aaed
timkim <timk@adobe.com>
2015-10-22 13:57:43 -0700

    [ci] - fixing pathing issue

--- resources/buildbox/build-ios.sh
+++ resources/buildbox/build-ios.sh
@@ -2,6 +2,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo "DIR is '$DIR'"
 git clone https://github.com/phonegap/phonegap-app-developer-keys ../../phonegap-app-developer-keys
+ls ../../resources/signing
 cp -r ../../phonegap-app-developer-keys/keys/ios/ ../../resources/signing/ios
 cat ../../resources/signing/ios/build-config.json
 pwd
