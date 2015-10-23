304e8bfeeb08b5457f53d3c8d21697b06895cf9a
timkim <timk@adobe.com>
2015-10-23 14:05:52 -0700

    [ci] - fixed things server side and cleaned script back up

--- resources/buildbox/build-ios.sh
+++ resources/buildbox/build-ios.sh
@@ -1,11 +1,2 @@
 #!/usr/bin/env bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-echo "DIR is '$DIR'"
-git clone https://github.com/phonegap/phonegap-app-developer-keys ../../phonegap-app-developer-keys
-ls 
-ls ../../
-ls ../resources/signing
-cp -r ../../phonegap-app-developer-keys/keys/ios/ ../../resources/signing/ios
-cat ../../resources/signing/ios/build-config.json
-pwd
 npm run -- release-ios
