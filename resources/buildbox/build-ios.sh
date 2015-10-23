4589db9361365ae64d92d70972b61a95886ffd46
Suraj Pindoria <suraj.pindoria@yahoo.com>
2015-10-23 15:32:23 -0700

    [ci] - add quotes around local file path

--- resources/buildbox/build-ios.sh
+++ resources/buildbox/build-ios.sh
@@ -9,4 +9,4 @@ export BUILDKITE_S3_ACCESS_KEY_ID=$AWSACCESSKEY
 export BUILDKITE_S3_SECRET_ACCESS_KEY=$AWSSECRETKEY
 export BUILDKITE_S3_DEFAULT_REGION=us-east-1
 export BUILDKITE_S3_ACL=private
-buildkite-agent artifact upload $IPALOCATION s3://ci.phonegap.com/phonegap-app-developer/ios/Phonegap-$BUILDKITE_COMMIT.ipa
+buildkite-agent artifact upload "$IPALOCATION" s3://ci.phonegap.com/phonegap-app-developer/ios/Phonegap-$BUILDKITE_COMMIT.ipa
