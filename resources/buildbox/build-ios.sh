4cb6fe6bd94ed122955c8d1b703be078d60fa1ef
Suraj Pindoria <suraj.pindoria@yahoo.com>
2015-10-23 14:58:43 -0700

    [ci] - have BuildKite upload iOS builds to S3

--- resources/buildbox/build-ios.sh
+++ resources/buildbox/build-ios.sh
@@ -1,2 +1,12 @@
 #!/usr/bin/env bash
-npm run -- release-ios
+npm run release-ios
+
+# upload binary to S3
+
+IPALOCATION=platforms/ios/build/device/PhoneGap.ipa
+
+export BUILDKITE_S3_ACCESS_KEY_ID=$AWSACCESSKEY
+export BUILDKITE_S3_SECRET_ACCESS_KEY=$AWSSECRETKEY
+export BUILDKITE_S3_DEFAULT_REGION=us-east-1
+export BUILDKITE_S3_ACL=private
+buildkite-agent artifact upload $IPALOCATION s3://ci.phonegap.com/phonegap-app-developer/ios/Phonegap-$BUILDKITE_COMMIT.ipa
