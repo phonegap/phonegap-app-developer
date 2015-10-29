5e93813fca488607783c134cbf85ba6a9e7707dc
Suraj Pindoria <suraj.pindoria@yahoo.com>
2015-10-29 16:16:05 -0700

    [ci] - generalize HockeyApp upload script to Android and iOS

--- resources/buildbox/build-ios.sh
+++ resources/buildbox/build-ios.sh
@@ -10,3 +10,6 @@ export BUILDKITE_S3_SECRET_ACCESS_KEY=$AWSSECRETKEY
 export BUILDKITE_S3_DEFAULT_REGION=us-east-1
 export BUILDKITE_S3_ACL=private
 buildkite-agent artifact upload "$IPALOCATION" s3://ci.phonegap.com/phonegap-app-developer/ios/Phonegap-$BUILDKITE_COMMIT.ipa
+
+# upload binary to HockeyApp
+node ./resources/script/hockey-app.js $HOCKEY_APP_TOKEN $HOCKEY_APP_IOS_ID $BUILDKITE_COMMIT
