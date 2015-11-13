c5a69162642da6d44b77ea508ad1dda8da898ed3
timkim <timk@adobe.com>
2015-11-12 16:02:30 -0800

    [ci] - added conditional logic for release and adhoc build workflow

--- resources/buildbox/build-ios.sh
+++ resources/buildbox/build-ios.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
-npm run adhoc-ios
+if ["$(git rev-parse HEAD)" != "$(git rev-parse $(git describe --abbrev=0 --tags))"]; then
+    npm run adhoc-ios
+    IPALOCATION="platforms/ios/build/device/PG Nightly.ipa"
+
+    # upload binary to HockeyApp
+    node ./resources/script/hockey-app.js $HOCKEY_APP_TOKEN $HOCKEY_APP_IOS_ID $BUILDKITE_COMMIT
+else
+    npm run release-ios
+    IPALOCATION="platforms/ios/build/device/PhoneGap.ipa"
+fi
 
 # upload binary to S3
-
-IPALOCATION="platforms/ios/build/device/PG Nightly.ipa"
-
 export BUILDKITE_S3_ACCESS_KEY_ID=$AWSACCESSKEY
 export BUILDKITE_S3_SECRET_ACCESS_KEY=$AWSSECRETKEY
 export BUILDKITE_S3_DEFAULT_REGION=us-east-1
 export BUILDKITE_S3_ACL=private
 buildkite-agent artifact upload "$IPALOCATION" s3://ci.phonegap.com/phonegap-app-developer/ios/Phonegap-$BUILDKITE_COMMIT.ipa
-
-# upload binary to HockeyApp
-node ./resources/script/hockey-app.js $HOCKEY_APP_TOKEN $HOCKEY_APP_IOS_ID $BUILDKITE_COMMIT
