867000e35633d3648739ecbc6e687cbc82422c3a
tommy-carlos williams <tommy@devgeeks.org>
2016-11-23 11:21:29 +1100

    Prefer spread operator over Object.assign

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,16 +2,18 @@ import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 
 function pgbReducer(state = {}, action) {
-  console.log(action.type);
+  //console.log(action.type);
   switch (action.type) {
     case 'PGB_LOGIN_RECEIVED':
-      return Object.assign({}, state, {
+      return {
+        ...state,
         access_token: action.access_token,
-      });
+      };
     case 'PGB_APPS_RECEIVED':
-      return Object.assign({}, state, {
+      return {
+        ...state,
         apps: action.apps,
-      });
+      };
     default:
       return state;
   }
