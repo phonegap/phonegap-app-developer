16237211354e2e56910e4936cc842be86657ae5d
timkim <timk@adobe.com>
2016-12-08 15:22:25 -0800

    [#470] - debugging buildkite ci stuff

--- resources/script/adhoc-ios.sh
+++ resources/script/adhoc-ios.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-
-npm run build:setup:adhoc
+echo "Running: npm run build"
+npm run build
+echo "Adding hockey app"
 phonegap plugin add cordova-plugin-hockeyapp
+echo "Running: phonegap build ios"
 phonegap build ios --verbose --device --debug --buildConfig=./resources/signing/ios/build-config.json
-npm run build:config:restore
