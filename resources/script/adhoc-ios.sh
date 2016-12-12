caed6e7ef82e938ad70f3e7f08929a893fae1033
timkim <timk@adobe.com>
2016-12-12 14:39:54 -0800

    [#470] - make sure to update config.xml and then restore for adhoc builds

--- resources/script/adhoc-ios.sh
+++ resources/script/adhoc-ios.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 echo "Running: npm run build"
 npm run build
+
+echo "Running: setup-ad-hoc.js"
+node ./setup-ad-hoc.js
+
 echo "Adding hockey app"
 phonegap plugin add cordova-plugin-hockeyapp
+
 echo "Running: phonegap build ios"
 phonegap build ios --verbose --device --debug --buildConfig=./resources/signing/ios/build-config.json
+
+echo "Running: restore-config.js"
+node ./restore-config.js
