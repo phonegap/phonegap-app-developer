88c0a2e73d650024408ac01445bacc106175779f
filmaj <maj.fil@gmail.com>
2017-01-05 16:28:15 -0800

    [ci] lets see how the android build goes.

--- resources/script/adhoc-android.sh
+++ resources/script/adhoc-android.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
+set -eo pipefail
 
-npm run build:setup:adhoc
+echo "Running: npm run build"
+npm run build
+
+echo "Running: setup-ad-hoc.js"
+node ./resources/script/setup-ad-hoc.js
+
+echo "Adding hockey app"
 phonegap plugin add cordova-plugin-hockeyapp
+
+echo "Running: phonegap build android"
 phonegap build android --verbose --release --buildConfig=./resources/signing/android/ad-hoc/build-config.json
-npm run build:config:restore
+
+echo "Running: restore-config.js"
+node ./resources/script/restore-config.js
