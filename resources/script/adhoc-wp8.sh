33cdf1ec9811c720cff3ab458342f78c4ffa2512
timkim <timk@adobe.com>
2017-08-22 16:11:42 -0700

    [#499] - remove .sh file

--- resources/script/adhoc-wp8.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-npm run build:setup:adhoc
-phonegap --verbose build wp8
-phonegap compile wp8 --release --verbose
-npm run build:config:restore
--- resources/script/phonegap-wp8.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-npm run build:setup
-phonegap --verbose build wp8
-npm run build:config-wp8
