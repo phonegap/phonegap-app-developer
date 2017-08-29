151e664b71b77969756b2717fa11b7fc97af4f41
timkim <timk@adobe.com>
2017-08-29 11:24:10 -0700

    [#499] - fixing up function brackets

--- resources/script/remove-hockeyapp.js
+++ resources/script/remove-hockeyapp.js
@@ -10,32 +10,33 @@ module.exports = function(ctx) {
 
     // modify app.js according to current platform
     var appDest = path.join(ctx.opts.projectRoot, 'www/js/app.js');
-    fs.readFile(appDest, 'utf8', function(err, data) {
-        if (err) {
-            console.log('Error reading app.js');
-            console.log('More info: <', err.message, '>');
-            deferral.reject(err);
+        fs.readFile(appDest, 'utf8', function(err, data) {
+            if (err) {
+                console.log('Error reading app.js');
+                console.log('More info: <', err.message, '>');
+                deferral.reject(err);
+            }
+
+        // delete hockeyapp code from app.js
+        var result = '';
+        if ((/%HOCKEYAPP([\s\S]*?)(%ENDHOCKEYAPP)/).test(data)) {
+            result = data.replace(/%HOCKEYAPP([\s\S]*?)(%ENDHOCKEYAPP)/, '%HOCKEYAPP');
+        } else {
+            console.log('Exiting: no HockeyApp code to remove');
+            deferral.resolve();
         }
 
-    // delete hockeyapp code from app.js
-    var result = '';
-    if ((/%HOCKEYAPP([\s\S]*?)(%ENDHOCKEYAPP)/).test(data)) {
-        result = data.replace(/%HOCKEYAPP([\s\S]*?)(%ENDHOCKEYAPP)/, '%HOCKEYAPP');
-    } else {
-        console.log('Exiting: no HockeyApp code to remove');
-        deferral.resolve();
-    }
+        // write back to app.js
+        fs.writeFile(appDest, result, 'utf8', function(err) {
+            if (err) {
+                console.log('Error while writting to app.js');
+                console.log('More info: <', err.message, '>');
+                deferral.reject(err);
+            }
 
-    // write back to app.js
-    fs.writeFile(appDest, result, 'utf8', function(err) {
-        if (err) {
-            console.log('Error while writting to app.js');
-            console.log('More info: <', err.message, '>');
-            deferral.reject(err);
-        }
-
-        deferral.resolve();
+            deferral.resolve();
+        });
     });
-    
+
     return deferral.promise;
 };
