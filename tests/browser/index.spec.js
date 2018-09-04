6c8c2252eae8b11456b150c3f25fcf0cec34ee00
Jesse MacFadyen <purplecabbage@gmail.com>
2018-09-04 13:20:28 -0700

    test cleanup, added eslint semistandard ... #somanynits

--- tests/browser/index.spec.js
+++ tests/browser/index.spec.js
@@ -1,6 +1,13 @@
+
 describe('PhoneGap Developer App', function() {
 
     it('runs and you are not insane', function() {
         expect(true).toBe(true);
     });
+
+    it('runs and you are not insane', function() {
+        expect(false).toBe(false);
+    });
+
+
 });
